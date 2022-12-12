57cb8a8d37d90171970b372c572dfa96ffa16f08
Chelsea Runacres <chelsea.runacres@gmail.com>
2022-12-12 14:39:23 -0500

    started code added

---
+++ server.js
@@ -0,0 +1,9 @@
+// return notes.html file
+
+// return all info w GET *
+
+// API routes:
+// GET/api/notes should read the db.json and return all saved notes as JSON
+
+// POST / api/notes should receive new note to save on the request body, add to the db.json file, and return the new route to the client 
+//      each note must have a unique id when its saved (look for npm package that could do this for me)
