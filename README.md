c29091a7b08c88fb83d0d5ee284551ace266568c
Chelsea Runacres <chelsea.runacres@gmail.com>
2022-12-13 11:15:33 -0500

    readme filled

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # note-taker
+
+## Description
+Store all your notes quickly and easily with this simple note taking app. This app was my first time working with express.js  on the backend. I hope you like it!
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://note-taker-supreme.herokuapp.com/)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+
+## Table of Contents (Optional)
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+N/A
+
+## Usage
+
+![Homepage-Screenshot](./public/assets/img/note-taker-screenshot1.png)
+![Homepage-Screenshot](./public/assets/img/note-taker-screenshot2.png)
+
+To use the note taker yourself, visit it [here](https://murmuring-sands-54732.herokuapp.com/)!
+
+
+## Credits
+N/A
+
+## License
+This work is covered by a MIT license, for more information read about the license [here](https://choosealicense.com/licenses/mit/)
+
+## How to Contribute
+If you have any suggestions or questions about the project, I'd love to hear from you! 
+
+Please reach out to me here on Github, at ChelsRunnn.
--- server.js
+++ server.js
@@ -3,37 +3,32 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const database = require('./db/db.json')
-
 // helper method for generating unique ids
 const uuid = require('./helpers/uuid');
-
 // Gives the express juice
 const app = express();
 // Port used when deployed || locally
 const PORT = process.env.PORT || 3001;
-
-
 // calling middleware 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 // ? ^^ what does this do?
 
+// HTML routes (could be moved to html route file?)
 // On page load, index.html
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html')));
-
 // Path to notes.html
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
-// API routes:
+// API routes (could be moved to api route file??)
 // Gets notes list from db file
 app.get('/api/notes', (req, res) => {
     res.json(database);
 })
-
 // Add new note to list
 app.post('/api/notes', (req, res) => {
     let jsonFilePath = path.join(__dirname, '/db/db.json');
@@ -52,33 +47,33 @@ app.post('/api/notes', (req, res) => {
             } else {
                 // Convert string into JSON object
                 const parsedNotes = JSON.parse(data);
-
                 // Add new note
                 parsedNotes.push(newNote);
-
                 // Write updated notes list back to db file
                 fs.writeFile('./db/db.json', JSON.stringify(parsedNotes, null, 4), (writeErr) =>
-                writeErr
-                ? console.error(writeErr)
-                : console.info('Successfully saved note!')
+                    writeErr
+                        ? console.error(writeErr)
+                        : console.info('Successfully saved note!')
                 );
             }
         });
-      const response = {
-        status: 'success!',
-        body: newNote,
-      };
-
-      console.log(response);
-      res.status(201).json(response);
+        const response = {
+            status: 'success!',
+            body: newNote,
+        };
+        console.log(response);
+        res.status(201).json(response);
     } else {
         res.status(500).json('Error in saving note');
     }
 });
+// Add delete note section (optional)
 
-// return all info w GET *
-// ?? ^^ how to do this without messing everything up
 
+// Wildcard route to send users to a index.html
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/index.html')));
+// Note telling where to access local host
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
